d58f26dbb532b3cce6fca977094ecbb3b355d2a6
emeka <emekaowo@yahoo.com>
2024-08-25 16:10:57 +0100

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode} from "react";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
